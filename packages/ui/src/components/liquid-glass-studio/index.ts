12b0ef9d707e82420597d8d3f3ebcb4b0d4f1dff
ccyang-aier <2095883812@qq.com>
2026-07-04 16:11:06 +0800

    refactor(ui): slim liquid glass studio component

--- packages/ui/src/components/liquid-glass-studio/index.ts
+++ packages/ui/src/components/liquid-glass-studio/index.ts
@@ -1,6 +1,3 @@
 'use client';
 
-import './index.scss';
-
-export { default as LiquidGlassStudio } from './App';
-export { default } from './App';
+export { default, LiquidGlassStudio } from './studio/LiquidGlassStudio';
